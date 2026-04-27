ac3f635f913e52b2aed435dccbf25822979ddb6e
alimrk <mrkprogramer04@gmail.com>
2026-04-27 17:20:56 +0330

    add featurs

--- persian mega corse/P-TO-DO/web-p-todo.py
+++ persian mega corse/P-TO-DO/web-p-todo.py
@@ -1,15 +1,25 @@
 import streamlit as st
 import P_functions
 
+todos = P_functions.read_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    P_functions.write_todos(todos)
 
 st.title("My To DO")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-todos = P_functions.read_todos()
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key= todo)
+    if checkbox:
+        todos.pop(index)
+        P_functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
-st.text_input(label="", placeholder= "add new todo ...")
-
+st.text_input(label="", placeholder= "add new todo ...", on_change= add_todo,
+            key= "new_todo")
