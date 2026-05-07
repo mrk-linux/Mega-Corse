55c31008ba0ab7fc0a5d3bfc4f77ef3d2075cb74
alimrk <mrkprogramer04@gmail.com>
2026-05-07 10:01:32 +0330

    send email

--- Portfolio Website/pages/Contact_us.py
+++ Portfolio Website/pages/Contact_us.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.header("Contact us")
+
+with st.form(key = "email_forms"):
+    user_email = st.text_input("Your email address")
+    message = st.text_area("Your message")
+    button = st.form_submit_button("submit")
+
+    if button:
+        pass
---
+++ Portfolio Website/send_email.py
@@ -0,0 +1,21 @@
+import smtplib, ssl
+
+host = "smtp.gmail.com"
+port = 465
+"""Here is the English translation of your text:
+First, you need to activate your email's two-step verification. 
+Then, go to the app password section. Give your app a name, 
+then copy and paste the generated password."""
+
+username = "your email address"
+password = "The password provided by Gmail"
+
+"""Storing the password in this way is not secure or correct; it must be stored in an encrypted (hashed) form."""
+receiver = "your email address"
+context = ssl.create_default_context()
+
+message = ""
+
+with smtplib.SMTP_SSL(host, port ,context=context) as server:
+    server.login(username, password)
+    server.sendmail(username, receiver, message)
