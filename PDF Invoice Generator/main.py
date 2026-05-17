27b4b0ac479c43dde452917bd02b36008ad59ed1
alimrk <mrkprogramer04@gmail.com>
2026-05-17 08:57:09 +0330

    Invoice Generator

---
+++ PDF Invoice Generator/main.py
@@ -0,0 +1,30 @@
+from fpdf import FPDF
+from pathlib import Path
+import glob
+
+# Create a list of text filepaths
+filepaths = glob.glob("pdfs//*.txt")
+# Create one PDF file
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+for filepath in filepaths:
+    # Add a page to the PDF document for each text file
+    pdf.add_page()
+
+    # Get the filename without the extension
+    # and convert it to title case (e.g. Cat)
+    filename = Path(filepath).stem
+    name = filename.title()
+
+    # Add the name to the PDF
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=name, ln=1)
+
+    # Get the content of each text file
+    with open(filepath, "r") as file:
+        content = file.read()
+    # Add the text file content to the PDf
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
+
+pdf.output("Output.pdf")
