3c613e9399f42a819be1ecdc06cc346113bed2ea
alimrk <mrkprogramer04@gmail.com>
2026-06-02 10:40:43 +0330

    Return temperature for any station and date

--- your weather api/main.py
+++ your weather api/main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import pandas as pd
+
 
 app = Flask(__name__) 
 
@@ -7,10 +9,12 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/api/v1/<station>/<Date>")
-@app.route("/")
-def about(station, Date):
-    return render_template("about.html")
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    filename = "mega corse\your weather api\data-small\TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows= 20, parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"]== date]["   TG"].squeeze() / 10
+    return {"station": station, "date": date, "temperature": temperature}
 
 if __name__ == "__main__":  
-    app.run(debug=True) 
+    app.run(debug=True, port = 5001) 
