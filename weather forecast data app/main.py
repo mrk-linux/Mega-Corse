e6906a22fbac3f8196b085099037ff77b37a3963
alimrk <mrkprogramer04@gmail.com>
2026-06-07 09:49:25 +0330

    Add temperature range filtering to API and add image

--- weather forecast data app/main.py
+++ weather forecast data app/main.py
@@ -3,16 +3,31 @@ import plotly.express as px
 from backend import get_data
 
 
+# Add title, text input, slider, selectbox and subheader
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place:")
 days = st.slider("Forecat Days", min_value= 1, max_value= 5,
                 help= "Select the number of forecasted days")
-option = st.selectbox("Select data to view", "Temperature", "sky")
+option = st.selectbox("Select data to view", ["Temperature", "Sky"])
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(place, days, option)
+if place:
+    # Get the temperature/sky data
+    filtered_data = get_data(place, days)
 
-d, t = get_data(days)
+    if option == "Temperature":
 
-figure = px.line(x = d, y = t, labels = {"x": "Date", "y": "Temperature (c)"})
-st.plotly_chart(figure)
+        temperature = [item["main"]["temp"] for item in filtered_data]
+        dates = [item["dt_txt"] for item in filtered_data]
+        #Create a temperature plot
+        figure = px.line(x = dates, y = temperature, labels = {"x": "Date", "y": "Temperature (c)"})
+        st.plotly_chart(figure)
+
+    if option == "Sky":
+        images = {"Clear":r"D:\porgram\code\mega corse\weather forecast data app\images\clear.png",
+                "Rain":r"D:\porgram\code\mega corse\weather forecast data app\images\rain.png",
+                "Clouds":r"D:\porgram\code\mega corse\weather forecast data app\images\cloud.png",
+                "Snow":r"D:\porgram\code\mega corse\weather forecast data app\images\snow.png"}
+        sky_conditions = [item["weather"][0]["main"] for item in filtered_data]
+        image_paths = [images[condition] for condition in sky_conditions]
+        st.image(image_paths,width= 115)
