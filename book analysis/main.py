b559f4d16a4fe8c8750cf7e8dd78876a5163aa02
alimrk <mrkprogramer04@gmail.com>
2026-06-11 18:23:34 +0330

    add lern and lern 2

--- book analysis/main.py
+++
@@ -1,8 +0,0 @@
-import re
-
-with open("miracle-in-the-andes.txt", "r", encoding="utf-8") as file:
-    book = file.read()
-
-pattern = re.compile("Chapter [0-9]+")
-findings = re.findall(pattern, book)
-print(len(findings))
