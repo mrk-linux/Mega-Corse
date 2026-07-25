ed3057c9b8f5b943158948661908f59f39afe530
alimrk <mrkprogramer04@gmail.com>
2026-07-25 11:55:59 +0330

    setup django

--- django from/job_application/models.py
+++ django from/job_application/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class From(models.Model):
+    first_name = models.CharField(max_length= 80)
+    last_name = models.CharField(max_length= 80)
+    email = models.EmailField()
+    data = models.DateField()
+    occupation = models.CharField(max_length= 80)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
