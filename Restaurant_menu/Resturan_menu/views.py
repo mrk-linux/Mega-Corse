df6b072bb0d0d6a37393b1caad1c73f6c091a782
alimrk <mrkprogramer04@gmail.com>
2026-07-29 21:13:17 +0330

    Context in Django

--- Restaurant_menu/Resturan_menu/views.py
+++ Restaurant_menu/Resturan_menu/views.py
@@ -7,6 +7,10 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
 
+    def get_context_data(self, **kwargs):
+        context = {"meals": "pizza"}
+        return context
+
 class MenuItemDetail(generic.DateDetailView):
     model = Item
     template_name = "menu_item_detail.html"
